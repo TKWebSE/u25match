be8ff4270ebf3d347da3de3de5894284080852be
TKWebSE <writetechcode@yahoo.co.jp>
2025-07-25 23:07:38 +0900

    addMockUser

--- app/(home)/(tabs)/(explore)/index.tsx
+++ app/(home)/(tabs)/(explore)/index.tsx
@@ -1,25 +1,10 @@
-// app/home.tsx など
+// app/ExploreScreen.tsx など
 import UserCard from '@components/UserCard';
+import { users } from '@mock/exploreUserMock';
 import React from 'react';
 import { ScrollView } from 'react-native';
 
 export default function ExploreScreen() {
-  const users = [
-    {
-      name: 'さくら',
-      age: 24,
-      location: '東京',
-      imageUrl: 'https://randomuser.me/api/portraits/women/44.jpg',
-      isOnline: true,
-    },
-    {
-      name: 'たけし',
-      age: 28,
-      location: '大阪',
-      imageUrl: 'https://randomuser.me/api/portraits/men/45.jpg',
-      isOnline: false,
-    },
-  ];
 
   return (
     <ScrollView className="p-4">
---
+++ src/mock/exploreUserMock.ts
@@ -0,0 +1,16 @@
+export const users = [
+  {
+    name: 'さくら',
+    age: 24,
+    location: '東京',
+    imageUrl: 'https://randomuser.me/api/portraits/women/44.jpg',
+    isOnline: true,
+  },
+  {
+    name: 'たけし',
+    age: 28,
+    location: '大阪',
+    imageUrl: 'https://randomuser.me/api/portraits/men/45.jpg',
+    isOnline: false,//lastActiveAtにしたいが、一旦実際内容確認で残す
+  },
+];
